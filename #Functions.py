3275e715523ff657dfd0797ad5112cfed21ac09f
shaulleo <99772221+shaulleo@users.noreply.github.com>
2023-03-14 19:10:40 +0000

    File com as funções

---
+++ #Functions.py
@@ -0,0 +1,48 @@
+#Functions
+
+#Extract education level from customer name
+def extract_education(observation):
+    name_list = observation.split(' ')
+    if len(name_list) > 2:
+        if name_list[0] == 'Msc.':
+            education = 'Masters Degree'
+        elif name_list[0] == 'Bsc.':
+            education = 'Bachelor Degree'
+        else:
+            education = 'Doctoral Degree'
+    else:
+        education = 'Basic'
+    return education
+
+
+#Clean the customers name
+def clean_names(observation):
+    name_list = observation.split(' ')
+    if len(name_list) > 2:
+        name = str(name_list[1] +' '+ name_list[2])
+    else:
+        name = str(name_list[0] + ' '+ name_list[1])
+    return name
+
+
+#Plot histograms based on optimum number of bins
+def plot_histogram(df, column_name):
+    # extract the data from the specified column
+    data = df[column_name]
+    
+    # calculate the optimal number of bins using the Freedman-Diaconis rule
+    q75, q25 = np.percentile(data, [75 ,25])
+    iqr = q75 - q25
+    bin_width = 2 * iqr * len(data)**(-1/3)
+    num_bins = int((data.max() - data.min()) / bin_width)
+    
+    # plot the histogram with the optimal number of bins
+    plt.hist(data, bins=num_bins)
+    
+    # set the plot title and axis labels
+    plt.title('Histogram of ' + column_name)
+    plt.xlabel(column_name)
+    plt.ylabel('Frequency')
+    
+    # display the plot
+    plt.show()
